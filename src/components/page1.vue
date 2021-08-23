6455da9893dad0a24f12dee099440aaeaa00a1c2
Ssondos <sondos11waell12@gmail.com>
2021-08-23 21:57:19 +0300

    add new

--- src/components/page1.vue
+++ src/components/page1.vue
@@ -1,21 +1,4 @@
 <template>
-<<<<<<< HEAD
-
-	<div>
-		<form @submit.prevent>
-		
-		<h1>SIGN IN</h1>
-		<p style="color:darkgray">if you care registered user sign in now</p>
-		<input type="text" v-model="form.email" placeholder="your e-mail"><br>
-		<input type="password" v-model="form.password" placeholder="your password" ><br>
-		<a href="#" style="color:darkgray" class="so">forget password?</a><br>
-		<button @click="submit">sign in </button><br>
-		<p style="color:darkgray"> <a href="#" style="color:darkgray">Request access</a> to your clinic</p>
-		<p style="color:skyblue">JOIN AS A CLINIC</p>
-		
-		</form>
-    </div>
-=======
   <div>
     <form @submit.prevent>
       <h1>SIGN IN</h1>
@@ -30,27 +13,11 @@
       <p style="color: skyblue">JOIN AS A CLINIC</p>
     </form>
   </div>
->>>>>>> 915352fcddc8317101866feb6e9aeee53f41d4dd
 </template>
 <script>
+import axios from 'axios'
 export default {
   name: "page1",
-<<<<<<< HEAD
-  data(){
-	  return{
-		  form:{
-			  email:null,
-			  password:null,
-		  }
-	  }
-  },
-  methods:{
-	  submit(){
-		  console.log(this.form)
-	  }
-  }
-
-=======
   data() {
     return {
       form: {
@@ -61,10 +28,13 @@ export default {
   },
   methods: {
     login() {
-      console.log(this.form);
+      axios.post('https://test-api.bradex.com/api/admin/login', this.form).then(res =>{
+        console.log(res.data);
+      }).catch(error => {
+        console.log(error.response.data);
+      })
     },
   },
->>>>>>> 915352fcddc8317101866feb6e9aeee53f41d4dd
 };
 </script>
 <style>
